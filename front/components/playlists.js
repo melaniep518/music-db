a7b219472499c6c759372ff8f6c280d7a11b20a5
Carlos Martinez <martinezcj2@gmail.com>
2016-12-07 11:35:12 -0500

    fixed playlits page

--- front/components/playlists.js
+++ front/components/playlists.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import $ from 'jquery';
 
+import {displayPlaylists} from '../actions/playlist-action';
+
 const Playlist = React.createClass({
   componentDidMount: function (){
     $.ajax({
       url: 'api/playlists',
       success: function (playlists) {
         console.log(playlists)
+        displayPlaylists(playlists)
       }
     })
   },
   render: function (){
+      console.log("playlists", this.props.playlists)
     return (
       <div className="mainContainer">
         <div className="artistContainer">
-          <h1>Playlists</h1>
-          <ul className="listUl">
-            <li className="list">Playlist names wil go here</li>
-            <li className="list">Playlist names wil go here</li>
+          <h1 className="pageTitles">PlayLits</h1>
+          <ul>
+            {this.props.playlists.map(function(val, idx) {
+            return <li key={idx}>{val.title}</li>
+            })}
           </ul>
         </div>
       </div>
